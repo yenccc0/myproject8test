4ab0da19e3e955a51e48fab095496ec5631e024a
mark455174@gmail.com <mark123456>
2022-06-10 12:59:43 +0800

    6/10 2nd commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("java")
+    kotlin("jvm") version "1.7.0"
 }
 
 group = "com.Mark"
@@ -12,8 +15,17 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
---
+++ src/main/java/com/Mark/PersonKotlin.kt
@@ -0,0 +1,13 @@
+package com.Mark
+
+class PersonKotlin {
+    fun greeting(){
+        println("Hello Kotlin")
+    }
+}
+
+//1st level
+fun main(){
+    val p = PersonKotlin()
+    p.greeting()
+}
